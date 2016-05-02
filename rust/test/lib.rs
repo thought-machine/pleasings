3242467437d5365a6fae1cc43edf2aa53a10d276
Peter Ebden <peter.ebden@gmail.com>
2016-05-02 14:22:48 +0100

    Initial beginning of Rust rules.

---
+++ rust/test/lib.rs
@@ -0,0 +1,15 @@
+// Extremely simple example taken straight from the Rust docs.
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(4, add_two(2));
+    }
+}
