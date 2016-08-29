b801121373a57d14fa3312906dd9c56190737114
Peter Ebden <peter.ebden@gmail.com>
2016-08-29 20:11:14 +0100

    Setting up CI with Travis. Needs some work on the grm rules to handle openfst not being installed.

---
+++ .travis_install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Provided for Travis, to download dependencies for their container
+# infrastructure and to set up a custom config.
+# See https://docs.travis-ci.com/user/migrating-from-legacy/ for details.
+
+set -eu
+
+cat <<EOF > .plzconfig.local
+[build]
+path = $PATH:$HOME/openfst/bin:$HOME/thrax/bin
+
+[go]
+goroot = $GOROOT
+
+[cpp]
+cctool = g++-4.8
+defaultoptcflags = --std=c++11 -O2 -DNDEBUG -I $HOME/openfst/include -L $HOME/openfst/lib -I $HOME/thrax/include -L $HOME/thrax/lib
+
+[cache]
+dir = $HOME/plz-cache
+
+EOF
+
+if [ ! -f "$HOME/openfst/bin/farextract" ]; then
+    rm -rf "$HOME/openfst"
+    curl -fSSL https://get.please.build/ci/openfst-1.5.4_linux_amd64.tar.gz | tar -xzC $HOME
+else
+    echo 'Using cached openfst'
+fi
+
+if [ ! -f "$HOME/thrax/bin/thraxcompiler" ]; then
+    rm -rf "$HOME/thrax"
+    curl -fSSL https://get.please.build/ci/thrax-1.5.4_linux_amd64.tar.gz | tar -xzC $HOME
+else
+    echo 'Using cached thrax'
+fi
