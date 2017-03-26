ef62d3415f0c51eb0bd0ddab853298878814c174
Peter Ebden <peter.ebden@gmail.com>
2017-03-26 13:56:11 +0100

    Starting to add a main

---
+++ grpc/test/main.go
@@ -0,0 +1,36 @@
+// Package main implements an example grpc-gateway server.
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	gw "grpc/test/kitten"
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterYourServiceHandlerFromEndpoint(ctx, mux, *echoEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":8080", mux)
+}
+
+func main() {
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
