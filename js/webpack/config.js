3a42958378d61dfe7ce91b1318c30b9110a331d5
Peter Ebden <peter.ebden@gmail.com>
2017-05-14 12:30:06 +0100

    Support for build envs

--- js/webpack/config.js
+++ js/webpack/config.js
@@ -3,6 +3,10 @@ const process = require('process');
 const webpack = require('webpack');
 const ClosureCompilerPlugin = require('webpack-closure-compiler');
 
+// Map plz's standard build config names to something meaningful to webpack.
+const buildConfig = process.env.BUILD_CONFIG;
+const nodeEnv = buildConfig === 'opt' ? 'production' : 'development';
+
 module.exports = {
     entry: process.env.SRCS.split(' ').map(src => './' + src),
     output: {
@@ -29,11 +33,15 @@ module.exports = {
 	modules: process.env.NODE_PATH.split(':'),
     },
     plugins: [
+	new webpack.DefinePlugin({
+	    'process.env': {
+		NODE_ENV: JSON.stringify(nodeEnv)
+	    }
+	}),
         new ClosureCompilerPlugin({
           compiler: {
             language_in: 'ECMASCRIPT6',
             language_out: 'ECMASCRIPT5',
-            compilation_level: 'ADVANCED'
           },
           concurrency: 3,
         })
