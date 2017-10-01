ac7da327c9425fb026893109dc898abe6e6bc7c6
Peter Ebden <peter.ebden@gmail.com>
2017-10-01 10:21:23 +0100

    Parameterising config

--- js/webpack/config.js
+++ js/webpack/config.js
@@ -1,86 +1,69 @@
 const fs = require('fs');
 const path = require('path');
-const process = require('process');
 const webpack = require('webpack');
 const requireDynamic = require('./require_dynamic.js');
 const BabelPresetES2015 = require('babel-preset-es2015');
 const BabelPresetReact = require('babel-preset-react');
 
+module.exports = function(opts) {
+    const library = opts.outManifest ? path.basename(opts.out, '.js') : undefined;
 
-// Map plz's standard build config names to something meaningful to webpack.
-const buildConfig = process.env.BUILD_CONFIG;
-const nodeEnv = buildConfig === 'opt' ? 'production' : 'development';
-
-let plugins = [
-    new webpack.DefinePlugin({
-	'process.env': {
-	    NODE_ENV: JSON.stringify(nodeEnv)
-	}
-    }),
-];
-
-let entry = process.env.SRCS_JS.split(' ').map(src => './' + src);
-let library = undefined;
-if (process.env.OUTS_MANIFEST) {
-    // We are building a vendor bundle,
-    const name = path.basename(process.env.OUTS_JS, '.js');
-    library = name;
-    plugins.push(new webpack.DllPlugin({
-        name: name,
-        path: path.join(process.env.TMP_DIR, process.env.OUTS_MANIFEST)
-    }));
-} else if (process.env.SRCS_DLL) {
-    // We have some vendor DLLs to link to.
-    const dlls = process.env.SRCS_DLL.split(' ');
-    const manifests = process.env.SRCS_MANIFEST.split(' ');
-    plugins = plugins.concat(dlls.map((dll, i) => new webpack.DllReferencePlugin({
-	name: path.basename(dll, '.js'),
-	manifest: JSON.parse(fs.readFileSync(manifests[i], 'utf8')),
-    })));
-}
-
-// Symlink this guy into place. For some reason it never seems to work to just add to modules.
-fs.symlinkSync(process.env.TOOLS_WEBPACK + '.buildin', process.env.TMP_DIR + '/buildin');
-
-module.exports = {
-    entry: entry,
-    output: {
-	path: process.env.TMP_DIR,
-        filename: path.basename(process.env.OUTS_JS),
-	library: library,
-    },
-    module: {
-	rules: [{
-	    test: /\.json$/,
-	    use: [{ loader: 'json-loader' }],
-	}, {
-	    test: /\.(js|jsx)$/,
-	    use: [
-		{
-		    loader: 'babel-loader',
-		    options: {
-			babelrc: false,
-			presets: [
-			    BabelPresetES2015,
-			    BabelPresetReact,
-			],
-		    },
-		}
+    return {
+	entry: opts.srcs.map(src => './' + src),
+	output: {
+	    path: opts.tmpDir,
+            filename: path.basename(opts.out),
+	    library: library,
+	},
+	module: {
+	    rules: [{
+		test: /\.json$/,
+		use: [{ loader: 'json-loader' }],
+	    }, {
+		test: /\.(js|jsx)$/,
+		use: [
+		    {
+			loader: 'babel-loader',
+			options: {
+			    babelrc: false,
+			    presets: [
+				BabelPresetES2015,
+				BabelPresetReact,
+			    ],
+			},
+		    }
+		],
+	    }]
+	},
+	node: {
+	    process: false,
+	    Buffer: false,
+	},
+	resolveLoader: {
+	    plugins: [requireDynamic.Resolver],
+	},
+	resolve: {
+	    modules: [
+		opts.tmpDir,
+		path.join(opts.tmpDir, 'third_party/js'),
 	    ],
-	}]
-    },
-    node: {
-	process: false,
-	Buffer: false,
-    },
-    resolveLoader: {
-	plugins: [requireDynamic.Resolver],
-    },
-    resolve: {
-	modules: [
-	    process.env.TMP_DIR,
-	    path.join(process.env.TMP_DIR, 'third_party/js'),
-	],
-    },
-    plugins: plugins,
+	},
+	plugins: [
+	    new webpack.DefinePlugin({
+		'process.env': {
+		    // Map plz's standard build config names to something meaningful to webpack.
+		    NODE_ENV: JSON.stringify(opts.buildConfig === 'opt' ? 'production' : 'development')
+		}
+	    }),
+	    ...opts.srcsDll.map((dll, i) => new webpack.DllReferencePlugin({
+		name: path.basename(dll, '.js'),
+		manifest: JSON.parse(fs.readFileSync(opts.srcsManifest[i], 'utf8')),
+	    })),
+	    ...(opts.outManifest ? [new webpack.DllPlugin({
+		name: library,
+		path: path.join(opts.tmpDir, opts.outManifest)
+	    })] : []),
+	]
+    };
+
 };
--- js/webpack/main.js
+++ js/webpack/main.js
@@ -2,7 +2,16 @@ const process = require('process');
 const webpack = require('webpack');
 const WebPackConfig = require('./config.js');
 
-webpack(WebPackConfig, function(err, stats) {
+webpack(WebPackConfig({
+    srcs: process.env.SRCS_JS.split(' '),
+    out: process.env.OUTS_JS,
+    srcsManifest: process.env.SRCS_MANIFEST ? process.env.SRCS_MANIFEST.split(' ') : [],
+    srcsDll: process.env.SRCS_DLL ? process.env.SRCS_DLL.split(' ') : [],
+    outManifest: process.env.OUTS_MANIFEST,
+    tmpDir: process.env.TMP_DIR,
+    buildConfig: process.env.BUILD_CONFIG,
+
+}), function(err, stats) {
     if (err) {
 	console.error(err.stack || err);
 	if (err.details) {
