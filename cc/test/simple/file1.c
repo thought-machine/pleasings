f80d2c956cf8b70545209b0b2eb2c96a35f5acdf
Peter Ebden <peter.ebden@gmail.com>
2017-12-09 22:15:06 +0000

    adding a simpler test

---
+++ cc/test/simple/file1.c
@@ -0,0 +1,8 @@
+extern int GetAnswer();
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  printf("%d\n", GetAnswer());
+  return 0;
+}
---
+++ cc/test/simple/file2.c
@@ -0,0 +1,3 @@
+int GetAnswer() {
+  return 42;
+}
