b224fc3e381c856c553114245dacf1087012fb11
Richard Godden <goddenrich@gmail.com>
2018-11-24 18:52:28 +0000

    templating seems to work but test doesnt

--- template/test/template_test.py
+++ template/test/template_test.py
@@ -1,9 +1,9 @@
 import unittest
-import subprocess
+import filecmp
 
 class templateTests(unittest.TestCase):
-    def test_yaml():
-        self.assertTrue(filecmp.cmp("expected.yaml", "generated.yaml"))
+    def test_yaml(self):
+        self.assertTrue(filecmp.cmp("template/test/expected.yaml", "generated.yaml"))
 
 
 
