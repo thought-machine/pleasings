d07bb901f28f7eb569268c5e02a454203e21c413
Richard Godden <goddenrich@gmail.com>
2018-11-24 19:38:25 +0000

    test working but failing on spaces

--- template/test/template_test.py
+++ template/test/template_test.py
@@ -1,10 +1,17 @@
 import unittest
 import filecmp
+import difflib
 
 class templateTests(unittest.TestCase):
     def test_yaml(self):
-        self.assertTrue(filecmp.cmp("template/test/expected.yaml", "generated.yaml"))
-
+        expected = 'template/test/expected.yaml'
+        generated = 'template/test/generated.yaml'
+        with open(expected) as e, open(generated) as g:
+            expected_text = e.readlines()
+            generated_text = g.readlines()
+            for diff in difflib.unified_diff(expected_text, generated_text, fromfile = expected, tofile = generated, n = 0):
+                print(diff)
+        self.assertTrue(filecmp.cmp(expected, generated))
 
 
 if __name__ == "__main__":
