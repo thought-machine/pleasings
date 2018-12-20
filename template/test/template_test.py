525de424c8b4ae0cd74bf67842a65954f9e0b7c3
Richard Godden <goddenrich@gmail.com>
2018-12-20 13:36:14 +0000

    substitutions is either list or dict

--- template/test/template_test.py
+++ template/test/template_test.py
@@ -3,9 +3,19 @@ import filecmp
 import difflib
 
 class templateTests(unittest.TestCase):
-    def test_yaml(self):
+    def test_yaml_dict(self):
         expected = 'template/test/expected.yaml'
-        generated = 'template/test/generated.yaml'
+        generated = 'template/test/generated_dict.yaml'
+        with open(expected) as e, open(generated) as g:
+            expected_text = e.readlines()
+            generated_text = g.readlines()
+            for diff in difflib.unified_diff(expected_text, generated_text, fromfile = expected, tofile = generated, n = 0):
+                print(diff)
+        self.assertTrue(filecmp.cmp(expected, generated))
+    
+    def test_yaml_list(self):
+        expected = 'template/test/expected.yaml'
+        generated = 'template/test/generated_list.yaml'
         with open(expected) as e, open(generated) as g:
             expected_text = e.readlines()
             generated_text = g.readlines()
