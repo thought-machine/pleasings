46e378e601069d1e06aeb8b1f96c984e0815ff13
Peter Ebden <peter.ebden@gmail.com>
2019-01-21 16:18:14 +0000

    working on the test rule

--- kotlin/test/lib.kt
+++ kotlin/test/lib.kt
@@ -1,4 +1,6 @@
-val answers: Map<String, Int> = hashMapOf(
+package test
+
+val questions: Map<String, Int> = hashMapOf(
     "What do you get if you multiply six by nine" to 42,
     "What do you get if you multiply six by seven" to 42
 );
